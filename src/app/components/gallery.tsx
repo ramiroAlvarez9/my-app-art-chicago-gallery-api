5cdca5a5ee2ee0d27546bd927963052832b06006
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2023-12-18 18:30:02 -0300

    adding hook use state, and use client. Fetching images from API

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -1,19 +1,39 @@
+"use client";
+import Image from "next/image";
+import { use, useState } from "react";
+
+ 
+type JSONresponse = any | JSON | Response;
+
 export default async function Gallery() {
 
-  const getGalleryInfo = await fetch( `https://api.artic.edu/api/v1/artworks/3752`, { cache: 'no-store' });
-  const data = await getGalleryInfo.json();
-  console.log(data.data.title);  
+  const [imagesInfoData, setimagesInfoData] = useState ();
 
-  return (
+  // general info from the gallery api, with 12 pictures
+  let getGeneralInfoFromGallery: Response = await fetch(`https://api.artic.edu/api/v1/artworks`,{ cache: "force-cache" });
+  let generalDataFromGalery: JSONresponse = await getGeneralInfoFromGallery.json();  
+  let dataArray = generalDataFromGalery.data;
+
+    //creating images calls 
+
+  dataArray.forEach ( async (element: any)  => {
+  
+    let getImagesInfo : Response = 
+    await fetch(
+      `https://api.artic.edu/api/v1/artworks/${element.id}?fields=id,title,image_id`,
+        { cache: "force-cache" }
+      ); 
+  
+  //esta linea va en el hook state!
+  //let imagesInfo = await getImagesInfo.json();  
+  })
+  
+  return(  
     <section>
-      
-    <h1>
-     
-      CONTENT
+   
 
-    </h1>
 
-    </section>     
-    )
+
+    </section>
+  )
 }
- 
