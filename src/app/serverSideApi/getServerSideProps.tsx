9cd77c886b399a8592f356b08143d15e4de2ebd7
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2023-12-19 21:04:52 -0300

    printing images in DOM from the fetch in api

---
+++ src/app/serverSideApi/getServerSideProps.tsx
@@ -0,0 +1,17 @@
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+
+type Repo = {
+  name: string;
+  stargazers_count: number;
+};
+
+export const getServerSideProps = (
+    async () => {
+ 
+      // Fetch data from external API
+        const res = await fetch(`https://api.artic.edu/api/v1/artworks?fields=id,title,image_id`);
+        const repo: Repo = await res.json();
+      // Pass data to the page via props'
+      return { props: { repo } };
+
+}) satisfies GetServerSideProps<{ repo: Repo }>;
